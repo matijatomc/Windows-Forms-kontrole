dade9ac61ad903711d0da9d2b091285acc40524e
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-01-25 15:01:07 +0100

    Add files via upload

---
+++ tomcMatijaDatetimePicker/tomcMatijaDatetimePicker/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace tomcMatijaDatetimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerDatum = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerVrijeme = new System.Windows.Forms.DateTimePicker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonRezerviraj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerDatum
+            // 
+            this.dateTimePickerDatum.Location = new System.Drawing.Point(239, 67);
+            this.dateTimePickerDatum.Name = "dateTimePickerDatum";
+            this.dateTimePickerDatum.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDatum.TabIndex = 0;
+            // 
+            // dateTimePickerVrijeme
+            // 
+            this.dateTimePickerVrijeme.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerVrijeme.Location = new System.Drawing.Point(239, 120);
+            this.dateTimePickerVrijeme.Name = "dateTimePickerVrijeme";
+            this.dateTimePickerVrijeme.ShowUpDown = true;
+            this.dateTimePickerVrijeme.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerVrijeme.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(108, 187);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 163);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Upišite datum leta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Upišite vrijeme polaska:";
+            // 
+            // buttonRezerviraj
+            // 
+            this.buttonRezerviraj.Location = new System.Drawing.Point(204, 356);
+            this.buttonRezerviraj.Name = "buttonRezerviraj";
+            this.buttonRezerviraj.Size = new System.Drawing.Size(140, 35);
+            this.buttonRezerviraj.TabIndex = 5;
+            this.buttonRezerviraj.Text = "Rezerviraj";
+            this.buttonRezerviraj.UseVisualStyleBackColor = true;
+            this.buttonRezerviraj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 418);
+            this.Controls.Add(this.buttonRezerviraj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dateTimePickerVrijeme);
+            this.Controls.Add(this.dateTimePickerDatum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatum;
+        private System.Windows.Forms.DateTimePicker dateTimePickerVrijeme;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonRezerviraj;
+    }
+}
+
---
+++ tomcMatijaDatetimePicker/tomcMatijaDatetimePicker/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tomcMatijaDatetimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            DateTime dt1 = dateTimePickerDatum.Value;
+            // objektu klase DateTime, dt1, dodjeljuje se vrijednost
+            // koju je korisnik izabrao u kontroli za datum
+            StringBuilder sb1 = new StringBuilder();
+            // objekt klase StringBuilder, sb1, koristi se za
+            // kreiranje stringa, koji ce se na kraju ispisati u richText // box-u
+            sb1.Append("Vas datum leta je:\n");
+            sb1.Append(dt1.ToShortDateString() + "\n");
+            // objektu klase DateTime, dt2, dodjeljuje se vrijednost
+            // koju je korisnik izabrao u kontroli za vrijeme
+            DateTime dt2 = dateTimePickerVrijeme.Value;
+            sb1.Append("Vase vrijeme polaska je:\n");
+            sb1.Append(dt2.ToShortTimeString());
+            // ispis kreiranog stringa u richText box-u
+            richTextBox1.AppendText(sb1.ToString());
+
+        }
+    }
+}
